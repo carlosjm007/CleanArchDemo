df54d023c1c646e5ae1d52a585ce1b92c211a313
carlos.manosalva <mano112374@siigo.com>
2020-02-24 12:44:54 -0500

    changes for presentation course controler, views, stalying etc

---
+++ CleanArch/CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArch.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseSerive;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseSerive = courseService;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseSerive.GetCourses();
+            return View(model);
+        }
+    }
+}
